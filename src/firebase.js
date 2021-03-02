1222908a7b95daafee3a7bcc936ad4273388f7b9
alperen <64660609+alperenkarate@users.noreply.github.com>
2021-03-03 00:19:00 +0300

    react-amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5WJI0ehfsteU6l7uEWDNeljVBR1FeCjg",
+    authDomain: "challange-fa4b7.firebaseapp.com",
+    projectId: "challange-fa4b7",
+    storageBucket: "challange-fa4b7.appspot.com",
+    messagingSenderId: "538031245580",
+    appId: "1:538031245580:web:1c302ff1bf554240f4e129"
+  };
